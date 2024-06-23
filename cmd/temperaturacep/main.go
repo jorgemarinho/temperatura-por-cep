b3c7e2127e2b89a41e8564f1074826d2c47d7450
Jorge Marinho <jorgeluizbsi@gmail.com>
2024-06-23 17:12:19 -0300

    correção

--- cmd/temperaturacep/main.go
+++ cmd/temperaturacep/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/jorgemarinho/temperatura-por-cep/internal/infra/web"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
-	http.HandleFunc("/busca-cep", BuscaCepHandler)
+	http.HandleFunc("/busca-cep", web.BuscaCepHandler)
 	http.ListenAndServe(":8080", nil)
 }
