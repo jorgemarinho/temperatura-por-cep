6af0e5d6fd26ed6699a3e6ff7a297f0e959b66c7
Jorge Marinho <jorgeluizbsi@gmail.com>
2024-07-01 16:56:54 -0300

    melhorando codigo

--- internal/usecase/busca_cep.go
+++ internal/usecase/busca_cep.go
@@ -1,109 +1,96 @@
 package usecase
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
+    "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
+    "net/url"
+    "os"
+    "regexp"
 
-	"github.com/jorgemarinho/temperatura-por-cep/internal/dto"
-	"github.com/jorgemarinho/temperatura-por-cep/internal/entity"
+    "github.com/jorgemarinho/temperatura-por-cep/internal/dto"
+    "github.com/jorgemarinho/temperatura-por-cep/internal/entity"
 )
 
+const (
+    viaCepURL  = "https://viacep.com.br/ws/%s/json/"
+    weatherAPI = "http://api.weatherapi.com/v1/current.json?key=%s&q=%s"
+)
+
+var weatherAPIKey = os.Getenv("WEATHER_API_KEY")
+
 type BuscaCepUseCase struct {
-	BuscaCepInputDTO dto.BuscaCepInputDTO
+    BuscaCepInputDTO dto.BuscaCepInputDTO
 }
 
 func NewBuscaCepUseCase(buscaCepInputDTO dto.BuscaCepInputDTO) *BuscaCepUseCase {
-	return &BuscaCepUseCase{
-		BuscaCepInputDTO: buscaCepInputDTO,
-	}
+    return &BuscaCepUseCase{
+        BuscaCepInputDTO: buscaCepInputDTO,
+    }
 }
 
 func (b BuscaCepUseCase) Execute() (dto.BuscaCepOutputDTO, error) {
+    if !isValidCep(b.BuscaCepInputDTO.Cep) {
+        return dto.BuscaCepOutputDTO{}, fmt.Errorf("CEP must have 8 digits and only contain numbers")
+    }
 
-	if len(b.BuscaCepInputDTO.Cep) < 8 {
-		err := fmt.Errorf("CEP must have 8 digits")
-		return dto.BuscaCepOutputDTO{}, err
-	}
+    cep, err := b.BuscaCep(b.BuscaCepInputDTO.Cep)
+    if err != nil {
+        return dto.BuscaCepOutputDTO{}, err
+    }
 
-	cep, err := b.BuscaCep(b.BuscaCepInputDTO.Cep)
+    temperatura, err := b.BuscaTemperatura(cep.Localidade)
+    if err != nil {
+        return dto.BuscaCepOutputDTO{}, err
+    }
 
-	if err != nil {
-		return dto.BuscaCepOutputDTO{}, err
-	}
-
-	temperatura, err := b.BuscaTemperatura(cep.Localidade)
-
-	if err != nil {
-		return dto.BuscaCepOutputDTO{}, err
-	}
-
-	return dto.BuscaCepOutputDTO{
-		TempC: temperatura.TempC,
-		TempF: getTemperatureKelvin(temperatura.TempF),
-		TempK: getTemperatureFahrenheit(temperatura.TempK),
-	}, nil
+    return dto.BuscaCepOutputDTO{
+        TempC: temperatura.TempC,
+        TempF: getTemperatureFahrenheit(temperatura.TempC),
+        TempK: getTemperatureKelvin(temperatura.TempC),
+    }, nil
 }
 
 func (b BuscaCepUseCase) BuscaCep(cep string) (*entity.Cep, error) {
-
-	resp, err := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-
-	if err != nil {
-		return nil, err
-	}
-
-	var c entity.Cep
-	err = json.Unmarshal(body, &c)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &c, nil
+    url := fmt.Sprintf(viaCepURL, cep)
+    return b.makeHTTPRequest[entity.Cep](url)
 }
 
 func (b BuscaCepUseCase) BuscaTemperatura(nomeCidade string) (*entity.Temperatura, error) {
+    encodedNomeCidade := url.QueryEscape(nomeCidade)
+    url := fmt.Sprintf(weatherAPI, weatherAPIKey, encodedNomeCidade)
+    return b.makeHTTPRequest[entity.Temperatura](url)
+}
 
-	apiKey := "8887ae192b2343f9a32114928240104"
-	returnNomeCidade := url.QueryEscape(nomeCidade)
-	url := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=%s", apiKey, returnNomeCidade)
-	resp, err := http.Get(url)
+func (b BuscaCepUseCase) makeHTTPRequest[T any](url string) (*T, error) {
+    resp, err := http.Get(url)
+    if err != nil {
+        return nil, fmt.Errorf("error making HTTP request: %w", err)
+    }
+    defer resp.Body.Close()
 
-	if err != nil {
-		return nil, err
-	}
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        return nil, fmt.Errorf("error reading response body: %w", err)
+    }
 
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
+    var result T
+    if err := json.Unmarshal(body, &result); err != nil {
+        return nil, fmt.Errorf("error unmarshalling response: %w", err)
+    }
 
-	if err != nil {
-		return nil, err
-	}
+    return &result, nil
+}
 
-	var t entity.Temperatura
-	err = json.Unmarshal(body, &t)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &t, nil
+func isValidCep(cep string) bool {
+    return regexp.MustCompile(`^\d{8}$`).MatchString(cep)
 }
 
 func getTemperatureFahrenheit(celsius float64) float64 {
-	return (celsius * 1.8) + 32
+    return (celsius * 1.8) + 32
 }
 
 func getTemperatureKelvin(celsius float64) float64 {
-	return celsius + 273
+    return celsius + 273.15
 }
